dfb69a64bcee5993b4333517fa9c7a832cbde8f9
DevilNickin <82429830+DevilNickin@users.noreply.github.com>
2022-05-05 12:09:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let si = document.getElementById('si')
+
+si.addEventListener("click", function(e) {
+    swal({
+        title: "Sabia que aceptarias namie linda ..... >3<",
+        icon: "succes",
+    });
+});
+
+let no = document.getElementById('no')
+
+no.addEventListener("mousemove", function(e){
+    let x = Math.round(Math.random()* 93);
+    let y = Math.round(Math.random()* 93);
+    console.log(x)
+    console.log(y);
+    no.style.left = x + "%";
+    no.style.top = y + "%"
+})
